cba3ade1166e183f48c3852d4769e5a73e696ae3
zhouguang <zhouguang@kenrobot.com>
2017-12-04 16:33:51 +0800

    translate

---
+++ locales/pen-msgs.js
@@ -0,0 +1,46 @@
+// GENERATED FILE:
+const penMsgs = {
+  "en": {
+    "messages": {
+      "pen.setColor": "set pen color to [COLOR]",
+      "pen.setColorParam": "set pen [COLOR_PARAM] to [VALUE]",
+      "pen.colorMenu.brightness": "brightness",
+      "pen.setSize": "set pen size to [SIZE]",
+      "pen.changeHue": "change pen hue by [HUE]",
+      "pen.stamp": "stamp",
+      "pen.clear": "clear",
+      "pen.setHue": "set pen hue to [HUE]",
+      "pen.setShade": "set pen shade to [SHADE]",
+      "pen.colorMenu.color": "color",
+      "pen.changeSize": "change pen size by [SIZE]",
+      "pen.colorMenu.transparency": "transparency",
+      "pen.changeColorParam": "change pen [COLOR_PARAM] by [VALUE]",
+      "pen.penDown": "pen down",
+      "pen.penUp": "pen up",
+      "pen.colorMenu.saturation": "saturation",
+      "pen.changeShade": "change pen shade by [SHADE]"
+    }
+  },
+  "zh": {
+    "messages": {
+      "pen.setColor": "设置画笔颜色为 [COLOR]",
+      "pen.setColorParam": "设置画笔 [COLOR_PARAM] 到 [VALUE]",
+      "pen.colorMenu.brightness": "亮度",
+      "pen.setSize": "设置画笔大小为 [SIZE]",
+      "pen.changeHue": "把画笔色调调到 [HUE]",
+      "pen.stamp": "图章",
+      "pen.clear": "清除",
+      "pen.setHue": "设置画笔色调为 [HUE]",
+      "pen.setShade": "设置画笔阴影为 [SHADE]",
+      "pen.colorMenu.color": "颜色",
+      "pen.changeSize": "把画笔大小调到 [SIZE]",
+      "pen.colorMenu.transparency": "透明度",
+      "pen.changeColorParam": "把画笔 [COLOR_PARAM] 调到 [VALUE]",
+      "pen.penDown": "画笔落下",
+      "pen.penUp": "画笔抬起",
+      "pen.colorMenu.saturation": "饱和度",
+      "pen.changeShade": "把画笔阴影调到 [SHADE]"
+    }
+  }
+}
+exports.messages = penMsgs;
